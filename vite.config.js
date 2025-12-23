814ea421eb86d24f81875c08921199706fc505dc
Luke Poley <81259213+polu4@users.noreply.github.com>
2025-12-23 12:41:14 -0700

    vite.config.js should work with custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/alpcpa_website/', // GitHub Pages repository name
+  base: '/',
+  plugins: [react()]
 })
